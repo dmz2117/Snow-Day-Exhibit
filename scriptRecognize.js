29efb5b1c07fc7141da000ee6268382bdade3774
David Zikovitz <dmz2117@tc.columbia.edu>
2021-12-10 13:15:26 -0500

    Begin setting up capture and trace

--- scriptRecognize.js
+++ scriptRecognize.js
@@ -3,7 +3,7 @@ let video;
 let detection=[];
 
 function setup() {
-  createCanvas(windowHeight*1.333, windowHeight);
+  createCanvas(windowWidth, windowHeight);
   video = createCapture(VIDEO);
   video.size(windowHeight*1.333, windowHeight);
   video.hide();
@@ -26,8 +26,14 @@ function gotResults(error, results) {
   doodleClassifier.classify(canvas, gotResults);
 }
 
+function finishImage() {
+  let myImg = get((windowWidth/2)-((windowHeight*1.333)/2), 0, windowHeight*1.333, windowHeight);
+  
+}
+
 function draw() {
-  image(video, 0, 0, windowHeight*1.333, windowHeight);
+  clear();
+  image(video, (windowWidth/2)-((windowHeight*1.333)/2), 0, windowHeight*1.333, windowHeight);
   filter(THRESHOLD, 0.5);
   fill(0);
   textSize(25);
@@ -38,13 +44,16 @@ function draw() {
     let guess3 = detection[2];
     let x=50;
     let y=50;
-    text('I mostley see a ' + guess1.label +' '+ round(guess1.confidence * 100, 0) +'%', x, y);
+    text('I mostly see a ' + guess1.label +' '+ round(guess1.confidence * 100, 0) +'%', x, y);
     textSize(20);
     text('or maybe a '+ guess2.label +' '+ round(guess2.confidence * 100, 0) +'%', x, y+30);
 
     if(guess1.confidence<0.3){
       textSize(25);
-      text('but I am really not sure!', x, y+60)
+      text("but I'm not really sure!", x, y+60)
     }
   }
+
+  
+
 }
